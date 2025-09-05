40dd05bc69a2c857de1af109431480fb47735a7f
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-05 16:31:48 -0300

    Create start.sh

---
+++ xpra-virt-manager/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "$HOSTS"
+dbus-launch gsettings set org.virt-manager.virt-manager.connections autoconnect "$HOSTS"
+dbus-launch gsettings set org.virt-manager.virt-manager xmleditor-enabled true
+
+trap 'exit 0' SIGTERM
+
+rm /tmp/.X100-lock
+
+while true;
+  do
+    sudo chmod 777 /var/run/libvirt/libvirt-sock
+    xpra start --bind-tcp=0.0.0.0:$PORTS,auth=password:value=$passw0rd --html=on --start=virt-manager --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 "$resolution"x24+32 -nolisten tcp -noreset" --pulseaudio=no --notifications=no --bell=no :100
+    sleep 5
+  done
